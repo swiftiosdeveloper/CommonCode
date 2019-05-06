37fc3d75b633870ef244335f3a8fd4132aeaa0db
JITESH <>
2019-05-07 02:10:58 +0530

    UIview Shadow
    UIview Shadow

--- SwiftExampleCode/SwiftExampleCode/Util/AppExtention/AppExtention.swift
+++ SwiftExampleCode/SwiftExampleCode/Util/AppExtention/AppExtention.swift
@@ -57,3 +57,43 @@ extension UIViewController {
     }
     
 }
+// #-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+//MARK:- UIView Extension
+// #-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+extension UIView {
+    
+    // OUTPUT 1
+    func dropShadow(scale: Bool = true) {
+        self.clipsToBounds = true
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: -1, height: 1)
+        self.layer.shadowRadius = 1
+        self.layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+    
+    // OUTPUT 2
+    func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true, shadowRedius : CGFloat = 1 ) {
+        self.clipsToBounds = true
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowOffset = offSet
+        self.layer.shadowRadius = shadowRedius
+        self.layer.cornerRadius = radius
+    }
+    
+    func dropShadowWithMask(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true, shadowRedius : CGFloat = 1 ) {
+        self.clipsToBounds = true
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowOffset = offSet
+        self.layer.shadowRadius = shadowRedius
+        self.layer.cornerRadius = radius
+    }
+    
+}
